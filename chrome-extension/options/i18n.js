28f0757c5fe5e7c2e7c64540ae0ac113bbb18694
Adel Qalieh <aqalieh95@gmail.com>
2013-11-19 22:06:51 -0500

    Clean up options page in Chrome extension

--- chrome-extension/options/i18n.js
+++ chrome-extension/options/i18n.js
@@ -16,8 +16,8 @@ this.i18n = {
     
     
     "information": {
-        "en": "Information",
-        "de": "Information"
+        "en": "Dropbox Information",
+        "de": "Dropbox Information"
     },
     "login": {
         "en": "Login",
@@ -40,13 +40,18 @@ this.i18n = {
         "de": "10 - 18 Zeichen"
     },
     "description": {
-        "en": "This is a description. You can write any text inside of this.<br>\
-        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut\
-        labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores\
-        et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem\
-        ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et\
-        dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.\
-        Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",
+        "en": "WARNING: This Chrome extension is still in beta, and may have some security problems.<br>\
+        This software is provided by the copyright holders and contributors AS IS\
+        and any express or implied warranties, including, but not limited to, the\
+        implied warranties of merchantibility and fitness for a particular purpose\
+        are disclaimed. In no event shall the copyright owner or contributors be\
+        liable for any direct, indirect, incidental, special, exemplary, or\
+        consequential damages (including, but not limited to, procurement of\
+        substitute goods or services; loss of use, data, or profits; or business\
+        interruption) however caused and on any theory of liability, whether in\
+        contract, strict liability, or tort (including negligence or otherwise)\
+        arising in any way out of the use of this software, even if advised of the\
+        possibility of such damage",
         
         "de": "Das ist eine Beschreibung. Du kannst hier beliebigen Text einfügen.<br>\
         Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut\
